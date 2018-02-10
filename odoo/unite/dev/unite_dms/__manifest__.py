f884038761d4ace255dbb57e53dbd389166dc3eb
n1k <be.istrefi@gmail.com>
2018-02-10 23:47:30 +0100

    unite_dms Module start

---
+++ odoo/unite/dev/unite_dms/__manifest__.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "unite_dms",
+
+    'summary': """
+        MUK_DMS and Odoo Documents combination by UNITE""",
+
+    'description': """
+        MUK_DMS and Odoo Documents combination by UNITE
+    """,
+
+    'author': "UNITE",
+    'contributors': 'Nik Istrefi',
+    'website': "http://www.uniteit.ch",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'UNITE',
+    'version': '2018.10.0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'muk_attachment_lobject',
+        'muk_dms',
+        'muk_dms_access',
+        'muk_dms_field',
+        'muk_dms_file',
+        'muk_fields_lobject',
+        'muk_web_client',
+        'muk_web_client_notification',
+        'muk_web_client_refresh',
+        'muk_web_export_attachment',
+        'muk_web_glyphicons',
+        'muk_web_preview',
+        'muk_web_preview_attachment',
+        'muk_web_preview_audio',
+        'muk_web_preview_csv',
+        'muk_web_preview_image',
+        'muk_web_preview_mail',
+        'muk_web_preview_markdown',
+        'muk_web_preview_msoffice',
+        'muk_web_preview_text',
+        'muk_web_preview_vector',
+        'muk_web_preview_video',
+        'muk_web_view_hierarchy',
+    ],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+}
+
+
---
+++ odoo/unite/dev/unite_dms/models/models.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class UniteDms(models.Model):
+    _name = 'unite.dms'
+    _inherit = 'muk_dms'

bb7b12748a69fd6778453b1668fe329feee35966
n1k <be.istrefi@gmail.com>
2018-04-20 02:19:13 +0200

    initialize unite_kb_category module

--- odoo/unite/dev/unite_kb_category/models/uniteKBCategory.py
+++ odoo/unite/dev/unite_kb_category/models/uniteKBCategory.py
@@ -9,6 +9,7 @@
 
 from odoo import models, fields, api
 
+
 class UniteKBCategory(models.Model):
     _name = 'unite.kb.category'
     _description = 'The Category Extension of Unite Knowledge Base'
@@ -19,3 +20,29 @@ class UniteKBCategory(models.Model):
     customerFriendly = fields.Boolean(string="Customer Friendly")
     catTags = fields.Many2one(comodel_name="unite.kb.category")
     catOwner = fields.Many2one(comodel_name="hr")
+
+    @api.one
+    def set_categoryTitle(self):
+        self.categoryTitle = self
+
+    @api.one
+    def set_subCategory(self):
+        self.subCategory = self
+
+    @api.one
+    def set_masterCategory(self):
+        self.masterCategory = self
+
+    @api.one
+    def set_customerFriendly(self):
+        self.customerFriendly = self
+
+    @api.one
+    def set_catTags(self):
+        self.catTags = self
+
+    @api.one
+    def set_catOwner(self):
+        self.catOwner = self
+
+    

a6243b04d2bc9b8863271657d23066425409b7d6
arlindsela <as21160@seeu.edu.mk>
2018-04-20 02:16:25 +0200

    First Class KB

--- odoo/unite/dev/unite_kb/models/uniteKB.py
+++ odoo/unite/dev/unite_kb/models/uniteKB.py
@@ -4,23 +4,23 @@ from odoo import models, fields, api
 
 
 class UniteKB(models.Model):
-    _name = 'unite_kb.UniteKB'
-    _inherits = 'unite_kb_category'
-    ArtTitle = fields.Char()
-    Author = fields.One2many()
-    CreatedAt = fields.Date()
+    _name = 'unite.kb'
+    _inherits = 'unite.kb.category'
+    ArtTitle = fields.Char(string="ArtTitle")
+    Author = fields.One2many(comodel_name="Author", string="Author")
+    CreatedAt = fields.Date(string="Date")
     fields.date.today()
-    Tags = fields.Many2one()
-    Description = fields.Text()
-    DevLang = fields.Many2one()
-    Version = fields.Integer()
-    Status = fields.Char()
-    LastEditedBy = fields.One2many()
-    LastEditedAt = fields.Date()
+    Tags = fields.Many2one(comodel_name="Tags", string="Tags")
+    Description = fields.Text(string="Description")
+    DevLang = fields.Many2one(comodel_name="DevLang")
+    Version = fields.Integer(string="Version")
+    Status = fields.Char(string="Status")
+    LastEditedBy = fields.One2many(string="LastEditedBy")
+    LastEditedAt = fields.Date(string="LastEditedAt")
     fields.date.today()
-    Problem = fields.Html()
-    Symptoms = fields.Html()
-    Solutions = fields.Html()
-    Inform = fields.Many2one()
+    Problem = fields.Html(string="Problem")
+    Symptoms = fields.Html(string="Symptoms")
+    Solutions = fields.Html(string="Solutions")
+    Inform = fields.Many2one(string="Inform")
 
 

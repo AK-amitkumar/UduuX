a1637cfe2482e749e543a7474d67157c2a14514a
arlindsela <as21160@seeu.edu.mk>
2018-04-22 22:14:17 +0200

    First Class KB

--- odoo/unite/dev/unite_kb/models/uniteKB.py
+++ odoo/unite/dev/unite_kb/models/uniteKB.py
@@ -5,17 +5,17 @@ from odoo import models, fields, api
 
 class UniteKB(models.Model):
     _name = 'unite.kb'
-    _inherits = 'unite.kb.category'
     ArtTitle = fields.Char(string="ArtTitle")
-    Author = fields.One2many(comodel_name="Author", string="Author")
+    #Author = fields.One2many(comodel_name="res.users", string="Author")
+    Author = fields.Char(string="Author")
     CreatedAt = fields.Date(string="Date")
     fields.date.today()
-    Tags = fields.Many2one(comodel_name="Tags", string="Tags")
+    #Tags = fields.Many2one(string="Tags")
     Description = fields.Text(string="Description")
-    DevLang = fields.Many2one(comodel_name="DevLang")
+    #DevLang = fields.Many2one(comodel_name="DevLang")
     Version = fields.Integer(string="Version")
     Status = fields.Char(string="Status")
-    LastEditedBy = fields.One2many(string="LastEditedBy")
+    LastEditedBy = fields.Char(string="LastEditedBy")
     LastEditedAt = fields.Date(string="LastEditedAt")
     fields.date.today()
     Problem = fields.Html(string="Problem")
@@ -24,3 +24,10 @@ class UniteKB(models.Model):
     Inform = fields.Many2one(string="Inform")
 
 
+    @api.one
+    def setAuthor(self):
+        self.Author = self.env.user
+
+    @api.onchange('Author')
+    def onchange_method(self):
+        self.LastEditedBy = self.Author
